4a6ada20f916620ac5dac18a6f56c2cb53e1e0a0
BirdiD <diioula.doucoure@gmail.com>
2023-12-04 09:44:56 +0100

    adding recordind feature

---
+++ src/pages/speech_recognition.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from st_pages import add_page_title, hide_pages
+
+add_page_title() 
+
+st.markdown(
+    """
+    Windanam is Cawoylel's first multidialectal speech recognition model. 
+    We encourage you test it and provides us some feedbacks on the model outputs
+"""
+)
+
+def main():
+    
+    return None
+
+
+if __name__ == "__main__":
+    main()
