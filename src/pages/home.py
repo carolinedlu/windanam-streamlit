f93a245d29f67e15d746b580e5bde51e9ffdb3ca
BirdiD <diioula.doucoure@gmail.com>
2023-12-07 10:47:16 +0000

    add language selection

--- src/pages/home.py
+++ src/pages/home.py
@@ -10,28 +10,57 @@ st.set_page_config(
 
 add_page_title() # By default this also adds indentation
 
+# Language selection radio button
+language_options = ["English", "French"]
+language = st.radio("Select language:", language_options)
+
+# Store selected language in session state
+st.session_state.language = language
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
-show_pages(
-    [
-        Page("src/pages/home.py", "About", "🏠"),
-        Page("src/pages/speech_recognition.py", "Speech Recognition", "🗣️"),
-        # Page("src/pages/translatation.py", "Translation", "🌐"),
-    ]
-)
 
+url = "https://join.slack.com/t/cawoylel/shared_invite/zt-27j4yeoc6-Lm9vptVwjIKqErMh3DQMiw"
 
-st.write("# 🇺🇸 Welcome to Cawoylel-Demo ! 👋")
+if st.session_state.language == "French":
+    show_pages(
+        [
+            Page("src/pages/home.py", "Accueil", "🏠"),
+            Page("src/pages/speech_recognition.py", "Windanam", "🗣️"),
+            # Page("src/pages/translatation.py", "Translation", "🌐"),
+        ]
+    )
+    st.write("# 🇫🇷 Bienvenue sur Cawoylel-Demo! 👋")
 
-st.markdown(
-    """
-    Cawoylel is a non-profit initiative dedicated to the development of technological and digital tools for the Fula language. 
-    Its goal is to address the challenges posed by the modern digital and technological revolution for African languages.
+    st.markdown(
+        """
+        Cawoylel est une initiative à but non lucratif qui se consacre à la conception d'outils technologiques et numériques pour la langue peule.
+        Notre objectif est de relever les défis posés par la révolution numérique et technologique moderne pour les langues africaines. 
 
-    On this page, you'll find demos of all the technologies that have been developed for the Fula language. We invite you to try them out and provide us your feedback.
-    
-    This project aims to be participatory. Individuals from diverse backgrounds, regardless of their expertise, are welcomed to actively contribute and shape meaningful solutions. 
-    Whether you are a language speaker, an engineer, a linguist, a sociologist, or have any other expertise, you are invited to join Cawoylel and be a part of its journey.
+        Sur ce site, vous trouverez toutes les démos des technologies qui ont été dévevoppée pour la langue peul. Nous vous invitons à les tester et nous faire vos retours.
 
-"""
-)
+        Cawoylel aspire à être une initiative participative. De ce fait, toute personne concernée peut rejoindre le mouvement, indépendamment de ses compétences : que vous soyez un simple locuteur de la langue, un ingénieur, un linguiste, un sociologue, ou autre. [Rejoignez le Slack](%s)   
+    """% url
+    )
+
+else:
+    show_pages(
+        [
+            Page("src/pages/home.py", "About", "🏠"),
+            Page("src/pages/speech_recognition.py", "Windanam - Speech Recognition", "🗣️"),
+            # Page("src/pages/translatation.py", "Translation", "🌐"),
+        ]
+    )
+    st.write("# 🇺🇸 Welcome to Cawoylel-Demo ! 👋")
+
+    st.markdown(
+        """
+        Cawoylel is a non-profit initiative dedicated to the development of technological and digital tools for the Fula language. 
+        Its goal is to address the challenges posed by the modern digital and technological revolution for African languages.
+
+        On this page, you'll find demos of all the technologies that have been developed for the Fula language. We invite you to try them out and provide us your feedback.
+        
+        This project aims to be participatory. Individuals from diverse backgrounds, regardless of their expertise, are welcomed to actively contribute and shape meaningful solutions. 
+        Whether you are a language speaker, an engineer, a linguist, a sociologist, or have any other expertise, you are invited to join Cawoylel and be a part of its journey. [Join Slack](%s).
+
+    """% url
+    )
--- src/pages/speech_recognition.py
+++ src/pages/speech_recognition.py
@@ -10,21 +10,51 @@ from transformers import pipeline
 
 add_page_title() 
 
-st.markdown(
-    """
-    **Windanam** is Cawoylel's first multidialectal speech recognition model. 
-    As with all AI systems, there are inherent risks that the model coud mis-transcribe what a person wants to say, or generate inaccurate outputs.
-    We encourage you to test it and provides us some feedbacks on the model outputs.
-"""
-)
-
 if "transcription" not in st.session_state:
     st.session_state.transcription = None
 
 if "audio" not in st.session_state:
     st.session_state.audio = None
 
-@st.cache_resource(show_spinner="Downloading model from Hugging Face...")
+st.session_state.mapping = {"French" : {"ressource_spinner" : "Téléchargement du modèle de reconnaisance vocale", 
+                                        "trancribe" : "Exécution du modèle. Cela peut prendre un certain temps", 
+                                        "fetching_output" : "Fetching output",
+                                        "output_label" : "Sortie du modèle",
+                                        "info_output" : "Pour nous aider à améliorer le modèle, vous pouvez cliquez sur le bouton ci-dessous afin de nous autoriser à utiliser vos enregistrements?",
+                                        "authorize_button" : "Autoriser l'utilisation de mes enregistrements",
+                                        "result_saved" : "Votre enregistrement a été bien pris en compte pour améliorer le modèle. Merci !",
+                                        "info_recording" : "Assurez-vous de vous être bien enregistré en cliquant sur le bouton Recording. Une fois enregistré, vous pourrez l'écouter. Attendez quelques secondes pour que le bouton Transcrire apparaisse. Cliquez dessus et attendez la sortie"
+                                        },
+
+"English" : {"ressource_spinner" : "Downloading model from Hugging Face...", 
+            "transcribe":"Running model. This can take some some time",
+            "fetching_output" : "Récupération des résultats",
+            "output_label" : "Model Output",
+            "info_output" : "The results might not be perfect yet. In order to improve the model, would you allow us to use your recordings? If yes, click on the button below",
+            "authorize_button" : "Allow my recordings to be used",
+            "result_saved" : "Your recording has been saved to improve the model. Thanks a lot!",
+            "info_recording" : "Make sure you have recorded yourself by clicking on Start Recording button. Once recorded, you will be able to listen to the recording. Wait some seconds for the Transcribe button to appear. Click on it and wait for the output"
+            }
+}  
+if st.session_state.language == "French":
+  st.markdown(
+        """
+        **Windanam** est le premier modèle de reconnaissance vocale multidialectale de Cawoylel.
+        Comme tous les systèmes d'intelligence artificielle, il existe des risques que le modèle mal interprète ce que veut dire une personne ou produise des résultats inexacts.
+        Nous vous encourageons à le tester et à nous faire vos retours afin de l'améliorer.
+    """
+    )
+else: 
+  st.markdown(
+      """
+      **Windanam** is Cawoylel's first multidialectal speech recognition model. 
+      As with all AI systems, there are inherent risks that the model coud mis-transcribe what a person wants to say, or generate inaccurate outputs.
+      We encourage you to test it and provides us some feedbacks on the model outputs.
+  """
+  )
+
+
+@st.cache_resource(show_spinner=st.session_state.mapping[st.session_state.language]["ressource_spinner"])
 def load_model(model_name = "cawoylel/windanam_mms-1b-tts_v2"):
   """
   Function to load model from hugging face
@@ -34,7 +64,7 @@ def load_model(model_name = "cawoylel/windanam_mms-1b-tts_v2"):
 
 pipeline = load_model()
 
-st.cache_data(show_spinner="Running model. This can take some some time")
+st.cache_data(show_spinner=st.session_state.mapping[st.session_state.language]["transcribe"])
 def transcribe_audio(sample):
   """
   Transcribe audio
@@ -48,25 +78,23 @@ def main():
     """
     wav_audio_data = st_audiorec()
     if wav_audio_data is not None:
-      st.audio(wav_audio_data, format='audio/wav')
       st.session_state.audio = wav_audio_data
-      if button("Transcribe recording", key="transcribe"):
+      if button("Transcribe", key="transcribe"):
         st.session_state.transcription = transcribe_audio(st.session_state.audio)
-        with st.spinner("Fetching output"):
-            st.text_area(label = "Model Output", 
+        with st.spinner(st.session_state.mapping[st.session_state.language]["fetching_output"]):
+            st.text_area(label = st.session_state.mapping[st.session_state.language]["output_label"], 
                              value=st.session_state.transcription, height = 100)
                 
         with st.container():
-          st.info("The results might not be perfect yet. In order to improve the model, would you allow us to use your recordings?", icon="ℹ️") 
-          if button("Allow my recordings to be used", key="saved"):
+          st.info(st.session_state.mapping[st.session_state.language]["info_output"], icon="ℹ️") 
+          if button(st.session_state.mapping[st.session_state.language]["authorize_button"], key="saved"):
             upload_to_drive(wav_audio_data)
             #Display a message indicating successful upload
-            st.success(f"Your recording has been saved to improve the model. A jaaraama")
+            st.success(st.session_state.mapping[st.session_state.language]["result_saved"])
 
                 
     else:
-      st.info("Make sure you have recorded yourself by clicking on Start Recording button. Once recorded, you will be able to listen to the recording. Wait some seconds for the Transcribe button to appear. Click on it and wait for the output")
-      st.warning("The process might induce some latencies", icon="⚠️")   
+      st.info(st.session_state.mapping[st.session_state.language]["info_recording"])
 
 
 if __name__ == "__main__":
